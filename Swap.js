34de606c7c814b7639571be780f9b46014b29b09
abhishek-lpu123 <63538834+abhishek-lpu123@users.noreply.github.com>
2022-01-28 13:08:57 +0530

    Add files via upload

---
+++ Swap.js
@@ -0,0 +1,12 @@
+let num1 = 5;
+let num2 = 10;
+ 
+console.log("The number before swapping are " +num1 , +num2);
+
+let num3;
+
+num3 = num2;
+num2 = num1;
+num1 = num3;
+
+console.log("The number after swapping are " +num1 , +num2);
